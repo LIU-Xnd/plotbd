0bb964ce80a5d069e61d30b5cad3a0535d6c7d45
LIU-Xnd <lqxnds@foxmail.com>
2025-05-09 16:08:15 +0800

    v0.2.0 - Plot in gridmesh.

--- src/plotbd.py
+++ src/plotbd.py
@@ -1,12 +1,15 @@
 import numpy as _np
 from numpy.typing import NDArray as _NDArray
 from dataclasses import dataclass as _dataclass
-from scipy.sparse import coo_matrix as _coo_matrix
+from scipy.sparse import csr_matrix as _csr_matrix
 from scipy.spatial.ckdtree import cKDTree as _cKDTree
 from tqdm import tqdm as _tqdm
+from typing import TypeAlias as _TypeAlias
 
-CategorizableType: type = _np.str_ | _np.int_
-NumberType: type = int | float
+CategorizableType: _TypeAlias = _np.str_ | _np.int_
+NumberType: _TypeAlias = int | float
+_Nx2ArrayType: _TypeAlias = _NDArray
+_1DArrayType: _TypeAlias = _NDArray
 
 @_dataclass
 class SpAnnPoints:
@@ -14,22 +17,22 @@ class SpAnnPoints:
     Spatial Annotated Points.
     
     Attrs:
-        coordinates (ndarray): a 2d-array of x, y of locations of points in space.
+        coordinates (_Nx2ArrayType): a 2d-array of x, y of locations of points in space.
         
-        masks (ndarray): a 1d-array of labels of points. Points of different labels are
+        masks (_1DArrayType): a 1d-array of labels of points. Points of different labels are
         considered to be of different objects.
     """
-    coordinates: _np.ndarray
-    masks: _NDArray[CategorizableType]
+    coordinates: _Nx2ArrayType
+    masks: _1DArrayType
     def __post_init__(self):
         assert len(self.masks) == self.coordinates.shape[0]
         assert self.coordinates.shape[1] == 2
         self._n_points: int = self.coordinates.shape[0]
-        self._xrange: tuple[NumberType] = (
+        self._xrange: tuple[NumberType, NumberType] = (
             self.coordinates[:,0].min(),
             self.coordinates[:,0].max(),
-        )
-        self._yrange: tuple[NumberType] = (
+)
+        self._yrange: tuple[NumberType, NumberType] = (
             self.coordinates[:,1].min(),
             self.coordinates[:,1].max(),
         )
@@ -74,13 +77,15 @@ def get_boundaries(
     ckdtree: _cKDTree = _cKDTree(
         data=ann_points.coordinates,
     )
-    dist_matrix: _coo_matrix = ckdtree.sparse_distance_matrix(
-        other=ckdtree,
-        max_distance=nbhd_radius,
-        p=2,
-        output_type="coo_matrix",
+    dist_matrix: _csr_matrix = _csr_matrix(
+        ckdtree.sparse_distance_matrix(
+            other=ckdtree,
+            max_distance=nbhd_radius,
+            p=2,
+        )
     )
-    boundary_masks: _NDArray[_np.bool_] = _np.zeros(
+    assert isinstance(dist_matrix, _csr_matrix)
+    boundary_masks: _1DArrayType = _np.zeros(
         shape=(ann_points._n_points,),
         dtype=bool,
     )
@@ -94,10 +99,10 @@ def get_boundaries(
     else:
         _itor = range(dist_matrix.shape[0])
     for i_point in _itor:
-        nbors: _NDArray[_np.int_] = dist_matrix.getrow(i_point).nonzero()[1]
+        nbors: _1DArrayType = dist_matrix.getrow(i_point).nonzero()[1]
         if len(nbors)==0:
             continue
-        masks_nbors_unique: _NDArray[CategorizableType] = _np.unique(ann_points.masks[nbors])
+        masks_nbors_unique: _1DArrayType = _np.unique(ann_points.masks[nbors])
         if len(masks_nbors_unique) > 1:
             boundary_masks[i_point] = True
             continue
@@ -107,13 +112,125 @@ def get_boundaries(
     return boundary_masks
 
 def plot_boundaries(
-    coordinates: _np.ndarray,
-    boundaries_flags: _NDArray[_np.bool_],
+    coordinates: _Nx2ArrayType,
+    boundaries_flags: _1DArrayType | None = None,
 ):
+    """Plot boundary points.
+    
+    Args:
+        coordinates (_Nx2ArrayType): coordinates of points. 1) If `boundaries_flags` is `None`,
+        then this parameter should be only boundary points; 2) otherwise, this should be
+        all raw points.
+        
+        boundaries_flags (_1DArrayType): boolean flags of boundary points if given, that is,
+        `coordinates[boundaries_flags, :]` are boundary points.
+    """
+    import seaborn as sns
+    if boundaries_flags is not None:
+        coordinates = coordinates[boundaries_flags, :]
+    return sns.scatterplot(
+            x=coordinates[:,0],
+            y=coordinates[:,1],
+        )
+    
+        
+def plot_boundaries_on_grids(
+    coordinates: _Nx2ArrayType,
+    boundaries_flags: _1DArrayType,
+    grid_density: int = 1000,
+):
+    """Plot boundary points (on a gridmesh).
+    
+    Args:
+        coordinates (_Nx2ArrayType): coordinates of points. This should be
+        all raw points for reference of grid anchors.
+        
+        boundaries_flags (_1DArrayType): boolean flags of boundary points if given, that is,
+        `coordinates[boundaries_flags, :]` are boundary points.
+
+        grid_density (int, optional): number of grids along the longest axis.
+    """
+    assert isinstance(boundaries_flags, _np.ndarray)
+    assert grid_density >= 1
+    if grid_density > 3000:
+        _tqdm.write(f'Warning: {grid_density=} > 3000 might be too large for RAM!')
+    xrange: tuple[NumberType, NumberType] = (coordinates[:,0].min(), coordinates[:,0].max())
+    yrange: tuple[NumberType, NumberType] = (coordinates[:,1].min(), coordinates[:,1].max())
+    xlen: NumberType = xrange[1] - xrange[0]
+    ylen: NumberType = yrange[1] - yrange[0]
+    assert xlen > 0
+    assert ylen > 0
+    if xlen >= ylen:
+        nx: int = grid_density
+        ny: int = max(1, int(_np.round(nx * ylen/xlen)))
+    else:
+        ny: int = grid_density
+        nx: int = int(_np.round(ny * xlen/ylen))
+    xs: _1DArrayType = _np.linspace(
+        start=xrange[0],
+        stop=xrange[1],
+        num=nx,
+        endpoint=True,
+        dtype=float,
+    )
+    ys: _1DArrayType = _np.linspace(
+        start=yrange[0],
+        stop=yrange[1],
+        num=ny,
+        endpoint=True,
+        dtype=float,
+    )
+    from itertools import product
+    anchor_points: _Nx2ArrayType = _np.array(
+        list(
+            product(
+                xs,
+                ys,
+            )
+        )
+    )
+    del xs
+    del ys
+    # Infer radius
+    area: NumberType = xlen * ylen
+    assert area > 0
+    # Area occupied by a point on average
+    inv_density: NumberType = area / coordinates.shape[0]
+    # Estimate nbhd_radius according to S=pi*r^2 -> r = sqrt(S/pi)
+    nbhd_radius: NumberType = 2 * _np.sqrt(inv_density/_np.pi)
+    # Increase it a bit
+    nbhd_radius *= 1.2
+
+    ckdtree_anchors = _cKDTree(
+        data=anchor_points,
+    )
+    ckdtree_points = _cKDTree(
+        data=coordinates,
+    )
+    dist_matrix: _csr_matrix = _csr_matrix(
+        ckdtree_anchors.sparse_distance_matrix(
+            other=ckdtree_points,
+            max_distance=nbhd_radius,
+            p=2,
+        )
+    )
+    anchor_masks: _1DArrayType = _np.zeros(
+        shape=(anchor_points.shape[0],),
+        dtype=_np.bool_,
+    )
+    for i_anchor in _tqdm(range(anchor_points.shape[0]), desc='Plotting anchors..', ncols=60,):
+        ix_nonzeros: _1DArrayType = dist_matrix.getrow(i_anchor).nonzero()[1]
+        entries_nonzeros: _1DArrayType = dist_matrix[i_anchor, ix_nonzeros].toarray().reshape(-1)
+        ix_min: int = ix_nonzeros[_np.argmin(entries_nonzeros)]
+        is_bd: bool = boundaries_flags[ix_min]
+        if is_bd:
+            anchor_masks[i_anchor] = is_bd
     import seaborn as sns
     return sns.scatterplot(
-        x=coordinates[:,0][boundaries_flags],
-        y=coordinates[:,1][boundaries_flags],
+        x=anchor_points[anchor_masks, 0],
+        y=anchor_points[anchor_masks, 1],
+        s=1,
     )
+
         
 
